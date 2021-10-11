79c5b1e41dd301eb4ac0beef2b39fb81c60b08d7
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-11 15:42:39 -0300

    Exibindo o valor da prop cardAttr2

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -23,6 +23,7 @@ class Card extends React.Component {
           <img data-testid="image-card" src={ cardImage } alt={ cardName } />
           <p data-testid="description-card">{cardDescription}</p>
           <p data-testid="attr1-card">{cardAttr1}</p>
+          <p data-testid="attr2-card">{cardAttr2}</p>
         </div>
       </section>
     );
