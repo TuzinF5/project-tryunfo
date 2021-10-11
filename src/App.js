bb8834e1eb6bbbd9368b6254bd1cac99a895fdbd
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-11 18:21:34 -0300

    Criando o estado incial e fazendo o bind nas funções

--- src/App.js
+++ src/App.js
@@ -5,6 +5,21 @@ import Form from './components/Form';
 class App extends React.Component {
   constructor() {
     super();
+    this.state = {
+      cardName: '',
+      cardDescription: '',
+      cardAttr1: '',
+      cardAttr2: '',
+      cardAttr3: '',
+      cardImage: '',
+      cardRare: '',
+      cardTrunfo: false,
+      hasTrunfo: false,
+      isSaveButtonDisabled: false,
+    };
+
+    this.onInputChange = this.onInputChange.bind(this);
+    this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
   }
 
   render() {
