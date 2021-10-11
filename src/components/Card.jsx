fde99a3173c49774539752d319394060c3899c47
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-11 15:18:58 -0300

    Fazendo checagem de tipos, recebidos das props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Card extends React.Component {
   render() {
@@ -12,8 +13,20 @@ class Card extends React.Component {
       cardRare,
       cardTrunfo,
     } = this.props;
+
     return <section> </section>;
   }
 }
+
+Card.propTypes = {
+  cardName: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
+  cardAttr1: PropTypes.string.isRequired,
+  cardAttr2: PropTypes.string.isRequired,
+  cardAttr3: PropTypes.string.isRequired,
+  cardImage: PropTypes.string.isRequired,
+  cardRare: PropTypes.string.isRequired,
+  cardTrunfo: PropTypes.bool.isRequired,
+};
 
 export default Card;
