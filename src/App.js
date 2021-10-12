abd54612b0c7cbd8f7e0803e61360f7448ce35f8
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-11 22:36:12 -0300

    Fazendo mençõs a links que consultei para resolver o codigo

--- src/App.js
+++ src/App.js
@@ -20,11 +20,12 @@ class App extends React.Component {
 
     this.onInputChange = this.onInputChange.bind(this);
     this.onSaveButtonClick = this.onSaveButtonClick.bind(this);
-    this.dataValidation = this.dataValidation.bind(this);
+    // this.dataValidation = this.dataValidation.bind(this);
   }
 
   onInputChange({ target: { name, value } }) {
-    this.setState({ [name]: value }, () => this.dataValidation());
+    this.setState({ [name]: value }, () => this.dataValidation()); // Link onde vi como eu precisava chamar minha função 'dataValidation()' => https://github.com/tryber/sd-013-a-project-tryunfo-beta/pull/20/commits/5c81b6008eb15e53c108061be34a89748bf1663c#:~:text=this.setState(%7B%20%5Bname%5D%3A%20value%20%7D%2C%20()%20%3D%3E%20this.buttonDisabled())%3B
+    // Link onde precisei entender o porque o setState recebe dois parametros => https://blog.matheuscastiglioni.com.br/atualizando-estado-dos-componentes-no-react/
   }
 
   onSaveButtonClick() {}
