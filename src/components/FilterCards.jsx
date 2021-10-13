1b001663cf5442d5934d0c644786bf776dbd3bab
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-13 01:22:01 -0300

    Recebendo props e fazendo checagem de tipos

--- src/components/FilterCards.jsx
+++ src/components/FilterCards.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class FilterCards extends React.Component {
   render() {
+    const { nameFilter, onInputChange } = this.props;
     return (
       <label htmlFor="name-filter">
         Filtre pelo nome da carta
@@ -10,10 +12,17 @@ class FilterCards extends React.Component {
           name="nameFilter"
           id="name-filter"
           data-testid="name-filter"
+          value={ nameFilter }
+          onChange={ onInputChange }
         />
       </label>
     );
   }
 }
+
+FilterCards.propTypes = {
+  nameFilter: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
 
 export default FilterCards;
