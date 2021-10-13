a5b8888f7c11c54296699101b2bf2c72ec1f2153
Arthur Teixeira <tuzinf5@gmail.com>
2021-10-13 02:03:10 -0300

    Recebendo props e fazendo checagem de tipos

--- src/components/RarityFilter.jsx
+++ src/components/RarityFilter.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class RarityFilter extends React.Component {
   render() {
+    const { rareFilter, onInputChange } = this.props;
     return (
-      <select
-        name="rareFilter"
-        id="rare-filter"
-        data-testid="rare-filter"
-      >
-        <option value="todas">todas</option>
-        <option value="normal">normal</option>
-        <option value="raro">raro</option>
-        <option value="muito raro">muito raro</option>
-      </select>
+      <label htmlFor="rare-filter">
+        Filtre por raridade da carta
+        <select
+          name="rareFilter"
+          id="rare-filter"
+          data-testid="rare-filter"
+          value={ rareFilter }
+          onChange={ onInputChange }
+        >
+          <option value="todas">todas</option>
+          <option value="normal">normal</option>
+          <option value="raro">raro</option>
+          <option value="muito raro">muito raro</option>
+        </select>
+      </label>
     );
   }
 }
+
+RarityFilter.propTypes = {
+  rareFilter: PropTypes.string.isRequired,
+  onInputChange: PropTypes.func.isRequired,
+};
 
 export default RarityFilter;
